1e65a33f60f3d7e695ffcc976a75cfb22b9793a5
Thoa Dinh <thoadinh@Thoas-MacBook-Air.local>
2022-08-15 16:13:13 +0700

    add change in file README.md and bikeshare.py

--- README.md
+++ README.md
@@ -16,3 +16,5 @@ Include the files used
 It's important to give proper credit. Add links to any repo that inspired you or blogposts you consulted.
 
 TEST COMMIT
+
+Addition change
--- bikeshare.py
+++ bikeshare.py
@@ -26,6 +26,7 @@ def get_filters():
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    Add comment 
     """
     print("-" * 40)
     print("Hello! Let's explore some US bikeshare data!")
